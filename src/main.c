7254b4c8ae0fb28739ec2ce64f22321eb6c9bf34
Anthony Suárez <neolight1010@gmail.com>
2021-05-03 21:28:14 -0500

    Change import order.

--- src/main.c
+++ src/main.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
 #include <gtk/gtk.h>
+#include <stdio.h>
 
 void activate(GtkApplication *app, gpointer data) {
-    GtkWidget *window;
+  GtkWidget *window;
 
-    window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "EcuRecetas");
-    gtk_window_set_default_size(GTK_WINDOW(window), 600, 450);
-    gtk_widget_show(window);
+  window = gtk_application_window_new(app);
+  gtk_window_set_title(GTK_WINDOW(window), "EcuRecetas");
+  gtk_window_set_default_size(GTK_WINDOW(window), 600, 450);
+  gtk_widget_show(window);
 }
 
 int main(int argc, char **argv) {
-    GtkApplication *app;
-    int status;
+  GtkApplication *app;
+  int status;
 
-    app = gtk_application_new("org.neolight.ecurecetas",
-                              G_APPLICATION_FLAGS_NONE);
-    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-    status = g_application_run(G_APPLICATION(app), argc, argv);
-    g_object_unref(app);
+  app =
+      gtk_application_new("org.neolight.ecurecetas", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+  status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
 
-    return status;
+  return status;
 }
